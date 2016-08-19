bf615016a209f0a6fb3dfe08399e15b3a91abace
megxkae xo <megxkae xo>
2016-08-19 16:17:00 -0700

    Show bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,39 @@
 //Business Logic//
 var letters = ["Ping", "Pong" , "Ping-pong"];
+var results;
+var inputNumber;
+var i;
 
-var pingPong = function(number){
-  var results = [];
-  var inputNumber = 1;
-
-  for (i=1; i<=number; i++) {
+var pingPong = function(userNumber){
+  results = [];
+  inputNumber = 1;
+  for (i=1; i<=userNumber; i++) {
     results.push(inputNumber);
     inputNumber ++;
-  //  alert(inputNumber);
     results.forEach(function(number){
-    if (number%15 === 0) {
-      number[i] = letters[0];
-    } else if (number%3 === 0) {
-      number[i] = letters[1];
-    } else if (number%5 === 0) {
-      number[i] = letters[2];
-    } else {
-      return number;
-    };
-  });
-};
+      if (number%15 === 0) {
+        number[i] = letters[1];
+      } else if (number%3 === 0) {
+        number[i] = letters[1];
+      } else if (number%5 === 0) {
+        number[i] = letters[2];
+      } else {
+        return number;
+      };
+    });
+  };
   return results;
 };
+
 //User Interface Logic//
 $(document).ready(function(){
   $("form#ping-pong").submit(function(event){
     event.preventDefault();
-    var number = parseInt($("input#number").val());
-    var result = pingPong(number);
-
-    $("#result").append("<li>" + result + "</li>");
+    var userNumber = parseInt($("input#number").val());
+    var input = pingPong(userNumber);
+    results = results.join(" ");
+      alert(results[3]);
+    $("#result").append("<li>" + results + "</li>");
     //$("#result").text(result);
-
   });
 });
