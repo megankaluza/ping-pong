81c148a5dbf5bfc1e5c23a0a90cea3905eb30004
megxkae xo <megxkae xo>
2016-08-19 11:29:23 -0700

    Show that my if else statement is outputting true or false.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 //Business Logic//
-var output = output;
-for(i=1;i<=20;++i);
 
-  if (i%3){
-    output = "Ping";
-  }
+var pingPong = function(number) {
+  if (number % 3 === 0){
+    return true;
+  } else {
+  return false;
+}
+};
 
+//var letters = ["Ping", "Pong" , "Ping-pong"];
 
-
-
+  //for(i=0;i<letters.length;i++){
+  //  if ()
+  //};
 
 //User Interface Logic//
 $(document).ready(function(){
-  $("form#logic").submit(function(event){
+  $("form#ping-pong").submit(function(event){
     event.preventDefault();
-    var output = $("#output").val();
+    var number = parseInt($("input#number").val());
+    var result = pingPong(number);
+    $("#result").text(result);
 
-  });
+});
 });
